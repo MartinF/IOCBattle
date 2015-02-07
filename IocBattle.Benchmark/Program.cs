9815bff0d037bde0cacb61cd16a90401921d98f2
Nathanael Mann <nathanael.mann@cit-design.co.uk>
2015-02-07 12:23:29 +0000

    Added simpleinjector

--- IocBattle.Benchmark/Program.cs
+++ IocBattle.Benchmark/Program.cs
@@ -19,6 +19,8 @@ namespace IocBattle.Benchmark
 
 									new StructureMapContainer(),
 
+                                    new SimpleInjectorContainer(), 
+
 			                 	new UnityContainer(),
 
 									new NinjectContainer(),
